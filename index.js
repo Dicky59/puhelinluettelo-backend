94cb4617bafbcb5e78f5779aa5188c927618d4d8
Pekka Laine <pekkaarvolaine@gmail.com>
2020-05-07 00:57:50 +0300

    puhelinluettelo_3.11

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('requestBody', function getRequestBody(req) {
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody', {
   skip: function (req, res) { return req.method != 'POST' }
