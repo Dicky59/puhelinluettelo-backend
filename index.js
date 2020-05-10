8168705399d75a019142237329c7ed331564071b
Pekka Laine <pekkaarvolaine@gmail.com>
2020-05-10 14:47:01 +0300

    puhelinluettelo_3.17

--- index.js
+++ index.js
@@ -70,6 +70,21 @@ app.post('/api/persons', (req, res, next) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
